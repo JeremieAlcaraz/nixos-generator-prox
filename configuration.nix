abb8154d2a725529648baea7ae1d6f2fac47ef74
Jeremie Alcaraz <hello@jeremiealcaraz.com>
2025-10-02 22:27:57 +0200

    ✨ feat(config): add git and tailscale support

--- configuration.nix
+++ configuration.nix
@@ -1,27 +1,39 @@
 { config, lib, pkgs, ... }:
 {
-  # Configuration minimale pour test
-  services.openssh.enable = true;
-  services.openssh.settings.PermitRootLogin = "yes";
-  
-  # Utilisateur root avec mot de passe simple pour test
-  users.users.root.password = "test123";
-  
-  # Quelques paquets de base
-  environment.systemPackages = with pkgs; [
-    vim
-    htop
-    curl
-    git
+  #### Réseau de base (Proxmox)
+  networking.hostName = "nixos-test";
+  networking.useNetworkd = true;                  # ou networking.networkmanager.enable = true;
+  networking.interfaces.ens18.useDHCP = true;     # explicite sur l'interface VirtIO par défaut
+
+  #### Cloud-init : désactivé (évite qu'il touche au DNS/réseau)
+  services.cloud-init.enable = false;
+
+  #### Tailscale
+  services.tailscale.enable = true;
+  services.tailscale.openFirewall = true;         # ouvre 41641/udp et règles nécessaires
+  services.tailscale.extraUpFlags = [
+    "--ssh"
+    "--accept-dns=false"                          # évite de casser la résolution tant que TS n'est pas loggé
+    # "--accept-routes=true"                     # active si tu veux recevoir des routes
+    # "--exit-node=<ID>" "--exit-node-allow-lan-access=true"
   ];
 
-  # Configuration réseau de base
-  networking.hostName = "nixos-test";
-  networking.useDHCP = lib.mkDefault true;
+  #### Kernel : interface TUN pour TS
+  boot.kernelModules = [ "tun" ];
 
-  # Configuration Tailscale minimale
-  services.tailscale.enable = true;
+  #### Firewall (optionnel mais utile avec exit nodes / subnet routes)
+  # networking.firewall.checkReversePath = "loose";
 
-  # Version du système
+  #### SSH (comme tu avais)
+  services.openssh.enable = true;
+  services.openssh.settings.PermitRootLogin = "yes";
+
+  #### Paquets
+  environment.systemPackages = with pkgs; [ vim htop curl git ];
+
+  #### (Sécurité) Mot de passe root de test - à changer ensuite !
+  users.users.root.password = "test123";
+
+  #### Version système
   system.stateVersion = "24.05";
 }
